b5e7b702a10bd1f3da0a3c6656a4460ffe38c00d
Le-Yo <koskeyhoward@gmail.com>
2018-07-17 12:50:44 +0300

    created more templates

--- app.py
+++ app.py
@@ -6,5 +6,27 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/lifestyle')
+def lifestyle():
+    return render_template('lifestyle.html')
+
+@app.route('/travel')
+def travel():
+    return render_template('travel.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+
+
+
 if __name__ == '__main__':
  	app.run(debug=True)
